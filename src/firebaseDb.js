f02653d47f3a1ffe2799c70bb0463dbae0b58e33
hmlu36 <hmlu36@gmail.com>
2020-11-18 16:59:00 +0800

    deploy

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase';
+import dotenv from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDM0QRhxR7kLgZUXOhzz1R8l3zCER1ZrVY",
-    authDomain: "lottery-3c582.firebaseapp.com",
-    databaseURL: "https://lottery-3c582.firebaseio.com",
-    projectId: "lottery-3c582",
-    storageBucket: "lottery-3c582.appspot.com",
-    messagingSenderId: "693340419747",
-    appId: "1:693340419747:web:f2643e073474e663367116",
-    measurementId: "G-W9LPGKREYP"
+    apiKey: process.env.APIKEY,
+    authDomain: `${process.env.DOMAIN}.firebaseapp.com`,
+    databaseURL: `https://${process.env.DOMAIN}.firebaseio.com`,
+    projectId: `${process.env.DOMAIN}`,
+    storageBucket: `${process.env.DOMAIN}.appspot.com`,
+    messagingSenderId: `${process.env.MESSAGINGSENDERID}`,
+    appId: `${process.env.APPID}`,
+    measurementId: `${process.env.MEASUREMENTID}`
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
