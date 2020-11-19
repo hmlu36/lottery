52386cd8902beba0a3580d249e20e063ce39cd99
hmlu36 <hmlu36@gmail.com>
2020-11-19 15:50:01 +0800

    deploy

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -1,19 +1,5 @@
 import firebase from 'firebase';
 
-
-/*
-const firebaseConfig = {
-    apiKey: "AIzaSyDM0QRhxR7kLgZUXOhzz1R8l3zCER1ZrVY",
-    authDomain: "lottery-3c582.firebaseapp.com",
-    databaseURL: "https://lottery-3c582.firebaseio.com",
-    projectId: "lottery-3c582",
-    storageBucket: "lottery-3c582.appspot.com",
-    messagingSenderId: "693340419747",
-    appId: "1:693340419747:web:f2643e073474e663367116",
-    measurementId: "G-W9LPGKREYP"
-};
-*/
-
 const firebaseConfig = {
     apiKey: process.env.VUE_APP_APIKEY,
     authDomain: `${process.env.VUE_APP_DOMAIN}.firebaseapp.com`,
@@ -24,7 +10,6 @@ const firebaseConfig = {
     appId: `${process.env.VUE_APP_APPID}`,
     measurementId: `${process.env.VUE_APP_MEASUREMENTID}`
 };
-console.log('env', JSON.stringify(process.env));
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const db = firebaseApp.firestore();
